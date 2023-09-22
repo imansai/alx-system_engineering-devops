2247ac4c34d0c02d0f8ff17e386dd5914e8024e8
imansai <im.saibari@gmail.com>
2023-09-22 11:43:57 -0700

    Fix puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #Create file named school in /tmp directory
 file { '/tmp/school':
-  ensure 	=> file,
-  content 	=> 'I love Puppet',
-  mode 		=> '0744',
-  owner		=> 'www-data',
-  group 	=> 'www-data',
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
