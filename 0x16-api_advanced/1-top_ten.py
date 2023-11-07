91eacb25352b734b8277c8baeac2d1ab6d294250
imansai <im.saibari@gmail.com>
2023-11-07 03:01:50 -0800

    Add 1-top_ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function that prints the 10 hottest posts on a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "alx.api.advanced:v1.0.0 (by /u/imannnnnnnnn)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
