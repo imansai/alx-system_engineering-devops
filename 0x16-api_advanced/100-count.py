1d0134d14e1d8ca97f4e0f1061ec56182ad97164
imansai <im.saibari@gmail.com>
2023-11-07 14:14:49 -0800

    Add documentation

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,6 +2,27 @@
 import requests
 
 def count_words(subreddit, word_list, counts=None, after=None):
+    """
+    Recursively queries the Reddit API, parses the titles of all hot articles,
+    and prints a sorted count of given keywords.
+
+    Args:
+        subreddit (str): The name of the subreddit to search.
+        word_list (list): A list of keywords to count in the post titles.
+        counts (dict, optional): A dictionary to store the counts of keywords.
+            Defaults to None.
+        after (str, optional): The ID of the last post in the current batch.
+            Defaults to None.
+
+    Returns:
+        None: This function does not return any value. It prints the results.
+
+    Note:
+        - The function works recursively and counts keywords in hot articles' titles.
+        - The results are printed in descending order by count and then sorted alphabetically.
+        - Words with no matches are skipped and not printed.
+        - Invalid subreddits may return a redirect to search results. No results are printed in this case.
+    """
     if counts is None:
         counts = {}
 
