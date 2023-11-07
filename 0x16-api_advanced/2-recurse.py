387619f1259ab4ebdb533c01e818cb554dd90c4e
imansai <im.saibari@gmail.com>
2023-11-07 13:44:59 -0800

    Correct 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,6 +12,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     params = {
         "after": after,
         "count": count,
+        "limit": 100
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
